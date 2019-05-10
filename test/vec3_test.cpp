330e97ba39274852ffa77869b2bc8336738db5a9
PenguinLemma <thepenguinlemma@gmail.com>
2019-05-10 19:42:03 +0200

    Add some example tests for Vec3

---
+++ test/vec3_test.cpp
@@ -0,0 +1,60 @@
+#include "vec3_test.hpp"
+
+namespace plemma {
+
+namespace glancy {
+
+bool AreApproxEqual(RealNum a, RealNum b, RealNum tol)
+{
+	return std::abs(b - a) < tol;
+}
+
+
+
+TEST_CASE("(0 0 0) is neutral element", "[Vec3][op +:Vec3 x Vec3 -> Vec3]")
+{
+	Vec3 zero(0.0, 0.0, 0.0);
+	auto v = GENERATE(take(100, RandomFiniteVec3()));
+	REQUIRE( v + zero == v );
+	REQUIRE( zero + v == v );
+}
+
+TEST_CASE("Operation + is associative", "[Vec3][op +:Vec3 x Vec3 -> Vec3]")
+{
+    auto u = GENERATE(take(100, RandomFiniteVec3()));
+    auto v = GENERATE(take(100, RandomFiniteVec3()));
+    REQUIRE( u + v == v + u );
+}
+
+TEST_CASE("Every element has an opposite", "[Vec3][op +:Vec3 x Vec3 -> Vec3]")
+{
+	Vec3 zero(0.0, 0.0, 0.0);
+    auto v = GENERATE(take(100, RandomFiniteVec3()));
+    REQUIRE( v + (-v) == zero );
+    v += -v;
+    REQUIRE( v == zero );
+}
+
+TEST_CASE("1.0 is neutral element", "[Vec3][op *: R x Vec3 -> Vec3]")
+{
+	auto v = GENERATE(take(100, RandomFiniteVec3()));
+	REQUIRE( 1.0 * v == v );
+}
+
+TEST_CASE("Multiplication by scalar well defined in terms of +", "[Vec3][op *: R x Vec3 -> Vec3]")
+{
+	auto v = GENERATE(take(100, RandomFiniteVec3()));
+	REQUIRE( 2.0 * v == v + v );
+}
+
+TEST_CASE("Vector is unitary after normilizing", "[Vec3][Norm]")
+{
+    auto v = GENERATE(take(100, RandomFiniteVec3()));
+	std::cout << v << std::endl;
+    v.Normalize();
+    REQUIRE(AreApproxEqual(v.Norm(), 1.0, kToleranceEqualityCheck));
+}
+
+} // namespace glancy
+
+} // namespace plemma
