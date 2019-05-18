8e6a0eae84b1980baa75c89e9d35b1d8cec3bb4f
PenguinLemma <thepenguinlemma@gmail.com>
2019-05-18 18:23:42 +0200

    Adapt random engine so that it can be receive a seed
    This will make testing/reproducing scenes possible :)

--- src/rand_engine.hpp
+++ src/rand_engine.hpp
@@ -8,9 +8,11 @@ namespace plemma
 namespace glancy
 {
 
-inline std::default_random_engine& my_engine()
+inline std::default_random_engine& my_engine(
+    unsigned int seed = static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count())
+    )
 {
-    static std::default_random_engine eng(static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count()));
+    static std::default_random_engine eng(seed);
 
     return eng;
 }
