4bd0e2555717b9473d29806d4d752d8899b872b4
PenguinLemma <thepenguinlemma@gmail.com>
2019-08-22 18:18:11 +0200

    constexpr a bit more

--- math/include/ray.hpp
+++ math/include/ray.hpp
@@ -11,13 +11,13 @@ namespace glancy
 class Ray
 {
 public:
-    Ray() noexcept = default;
-    Ray(const Vec3& origin, const Vec3& direction, RealNum t)
+    constexpr Ray() noexcept = default;
+    constexpr Ray(const Vec3& origin, const Vec3& direction, RealNum t)
         : origin_(origin), direction_(direction), time_(t) {}
-    inline Vec3 Origin() const { return origin_; }
-    inline Vec3 Direction() const { return direction_; }
-    inline RealNum Time() const { return time_; }
-    inline Vec3 PointAtParameter(RealNum lambda) const { return origin_ + lambda * direction_; }
+    constexpr Vec3 Origin() const { return origin_; }
+    constexpr Vec3 Direction() const { return direction_; }
+    constexpr RealNum Time() const { return time_; }
+    constexpr Vec3 PointAtParameter(RealNum lambda) const { return origin_ + lambda * direction_; }
 
 private:
     Vec3 origin_;
